b6b06c3063b98c04943f598d328f195e5883274b
andychung0214 <andy6756@gmail.com>
2022-08-05 10:30:22 +0800

    Add Firebase Module

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,13 @@
 var express = require('express');
 var router = express.Router();
 
+function checkFireBase(){
+  
+}
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
+  checkFireBase();
   res.render('index', { title: 'Express' });
 });
 
